da36beb41a0c140deb73bcfe8c47ed49481937ba
PhotoFont <sacktum@gmail.com>
2026-09-16 08:53:27 +0700

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./talubpra.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////app/talubpra.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
